04f43fa0fecbef0cd5ba89621c1a737a896046f5
jericodejesus <dejesusjerico528@gmail.com>
2025-12-05 05:31:18 +0800

    .env notworking on flutter

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,40 @@ import 'package:dignitywithcare/providers/client_provider.dart';
 import 'package:dignitywithcare/providers/document_provider.dart';
 import 'package:dignitywithcare/providers/manage_user_provider.dart';
 import 'package:dignitywithcare/providers/notes_provider.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'app_router.dart';
 import 'firebase_options.dart';
 import 'providers/auth_provider.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_storage/get_storage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();   // ← MUST come first
+  //
+  // if (!kIsWeb) {
+  //   await dotenv.load(fileName: ".env"); // Only mobile
+  // }     // ← NOW loads correctly
 
-  await dotenv.load(fileName: ".env");         // ← NOW loads correctly
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyCCzxcJ0OCgl5oe-n6S-73lgE1TGMqKcyU",
+        appId: "1:404595228914:web:5a5baa60241aac53db106b",
+        messagingSenderId: "404595228914",
+        projectId: "dignity-with-care",
+        storageBucket: "dignity-with-care.firebasestorage.app",
+        authDomain: "dignity-with-care.firebaseapp.com",
+        measurementId: "G-FQ18C33WQE",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
   await GetStorage.init();
 
   runApp(
